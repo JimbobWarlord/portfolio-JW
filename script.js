6505ab1f7b8ad7f63ca0bec54503dec3a2ee5239
JimbobWarlord <s4092060@student.rmit.edu.au>
2026-04-29 16:28:01 +1000

    b

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const audio = document.getElementById("my-audio");
+  const songs = document.querySelectorAll(".song-item");
+
+  songs.forEach((song) => {
+    song.addEventListener("click", function () {
+      const songUrl = this.getAttribute("data-src");
+      audio.src = songUrl;
+      audio.currentTime = 0;
+      audio.play();
+      audio.volume = 0.3;
+      console.log("bing");
+    });
+  });
+});
+
 window.addEventListener("load", () => {
   document.body.classList.add("loaded");
 });
